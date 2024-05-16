f15945c84447fbaafb72cd42951d831cf43cf845
RokFonovic <rok.fonovic@student.um.si>
2024-05-16 10:49:01 +0200

    Vzpostavitev zaledja in povezava na MongoDB

---
+++ nepremicnine/src/main/java/si/primerjanjeCen/nepremicnine/NepremicnineApplication.java
@@ -0,0 +1,13 @@
+package si.primerjanjeCen.nepremicnine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NepremicnineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NepremicnineApplication.class, args);
+	}
+
+}
---
+++ nepremicnine/src/test/java/si/primerjanjeCen/nepremicnine/NepremicnineApplicationTests.java
@@ -0,0 +1,13 @@
+package si.primerjanjeCen.nepremicnine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NepremicnineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
