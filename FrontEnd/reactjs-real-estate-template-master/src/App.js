31668e0d35f7771eaba0efbbf5aacb215306a7c4
carluka <carluka33@gmail.com>
2024-05-29 15:29:01 +0200

    primerjanje

--- FrontEnd/reactjs-real-estate-template-master/src/App.js
+++ FrontEnd/reactjs-real-estate-template-master/src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import "./App.css";
 import FlatDetail from "./components/FlatDetail";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
@@ -8,26 +8,27 @@ import About from "./components/About";
 import Blog from "./components/Blog";
 import BlogDetail from "./components/BlogDetail";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-
+import { ComparisonProvider } from "./other/ComparisonContext";
+import ComparisonPage from "./components/ComparisonPage";
 
 function App() {
-
-
   return (
-    <Router>
-      <div className="App">
-        <Header />
+    <ComparisonProvider>
+      <Router>
+        <div className="App">
+          <Header />
 
-        <Route path="/" exact component={Home} />
-        <Route path="/contact" component={Contact} />
-        <Route path="/about" component={About} />
-        <Route path="/blog" exact component={Blog} />
-        <Route path="/blog/:id" component={BlogDetail} />
-        <Route path="/nepremicnina/:id" component={FlatDetail} />
-    
-        <Footer />
-      </div>
-    </Router>
+          <Route path="/" exact component={Home} />
+          <Route path="/contact" component={ComparisonPage} />
+          <Route path="/about" component={About} />
+          <Route path="/blog" exact component={Blog} />
+          <Route path="/blog/:id" component={BlogDetail} />
+          <Route path="/nepremicnina/:id" component={FlatDetail} />
+
+          <Footer />
+        </div>
+      </Router>
+    </ComparisonProvider>
   );
 }
 
--- FrontEnd/reactjs-real-estate-template-master/src/components/FlatItem.js
+++ FrontEnd/reactjs-real-estate-template-master/src/components/FlatItem.js
@@ -1,37 +1,55 @@
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
+import { ComparisonContext } from "../other/ComparisonContext";
 
-// TUKAJ SE MORAJO STVARI PRIKAZOVATI DINAMOCNO
 const FlatItem = ({ nepremicnina }) => {
-  const { id, naziv, slug, cena, image_urls, posredovanje, agencija } =
-    nepremicnina;
+  const { id, naziv, cena, image_urls, posredovanje, agencija } = nepremicnina;
+  const { comparisonList, addToComparison } = useContext(ComparisonContext);
 
   const formattedCena =
     posredovanje === "Oddaja" ? `${cena}€/mesec` : `${cena}€`;
+
+  const handleAddToComparison = () => {
+    if (comparisonList.length >= 3) {
+      alert("Največje število primerjav je 3.");
+    } else {
+      addToComparison(nepremicnina);
+    }
+  };
+
   return (
-    <div className="text-center col-lg-4 col-12 col-md-6 ">
+    <div className="text-center col-lg-4 col-12 col-md-6">
       <div className="item">
         <div className="item-image">
-          {<img className="img-fluid" src={image_urls[0]} alt={naziv} />}
+          <img className="img-fluid" src={image_urls[0]} alt={naziv} />
         </div>
         <div className="item-description">
-          <div className="d-flex justify-content-between mb-3">
-            <span className="item-title">{naziv}</span>
-            <span className="item-price">{formattedCena}</span>
+          <div className="item-title">
+            <span>{naziv}</span>
+            <span>{formattedCena}</span>
           </div>
-          <div className="item-icon d-flex alig-items-center justify-content-between">
+          <div className="item-icon">
             <div>
               <i className="fas fa-check-circle"></i>{" "}
               <span>{posredovanje}</span>
             </div>
             <div>
-              <i className="fas fa-check-circle"></i> <span> {agencija} </span>
+              <i className="fas fa-check-circle"></i> <span>{agencija}</span>
             </div>
+          </div>
+          <div className="item-buttons">
             <Link
               to={{ pathname: `/nepremicnina/${id}`, state: { nepremicnina } }}
               className="item-title"
             >
               <button className="btn btn-detail">Poglej</button>
             </Link>
+            <button
+              onClick={handleAddToComparison}
+              className="btn btn-comparison"
+            >
+              <i className="fas fa-exchange-alt"></i> Primerjaj
+            </button>
           </div>
         </div>
       </div>
