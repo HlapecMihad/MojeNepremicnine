f7798603999dcf7688930e3ca2d368c1300af148
carluka <carluka33@gmail.com>
2024-06-04 12:51:11 +0200

    merge

--- FrontEnd/reactjs-real-estate-template-master/src/components/FlatList.js
+++ FrontEnd/reactjs-real-estate-template-master/src/components/FlatList.js
@@ -18,32 +18,82 @@ const FlatList = ({ filters = {} }) => {
         console.error("There was an error fetching the nepremicnine!", error);
       });
   }, [addNepremicnine]);
-  }, [addNepremicnine]);
 
   const applyFilters = (properties) => {
     return properties.filter((property) => {
-      const cena = parseFloat(property.cena.replace(/[.,]/g, ''));
-      const velikostSkupaj = parseFloat(property.velikost_skupaj.split(" ")[0].replace(",", "."));
-      const velikostZemljisca = parseFloat(property.velikost_zemljisca.split(" ")[0].replace(",", "."));
+      const cena = parseFloat(property.cena.replace(/[.,]/g, ""));
+      const velikostSkupaj = parseFloat(
+        property.velikost_skupaj.split(" ")[0].replace(",", ".")
+      );
+      const velikostZemljisca = parseFloat(
+        property.velikost_zemljisca.split(" ")[0].replace(",", ".")
+      );
       const lokacijaDel = property.lokacija.split(", ")[0];
 
-      if (filters.posredovanje && property.posredovanje !== filters.posredovanje) return false;
-      if (filters.tip_nepremicnine && property.tip_nepremicnine !== filters.tip_nepremicnine) return false;
+      if (
+        filters.posredovanje &&
+        property.posredovanje !== filters.posredovanje
+      )
+        return false;
+      if (
+        filters.tip_nepremicnine &&
+        property.tip_nepremicnine !== filters.tip_nepremicnine
+      )
+        return false;
       if (filters.lokacija && lokacijaDel !== filters.lokacija) return false;
       if (filters.cenaMin !== null && cena < filters.cenaMin) return false;
       if (filters.cenaMax !== null && cena > filters.cenaMax) return false;
-      if (filters.st_sob !== null && property.st_sob !== filters.st_sob) return false;
-      if (filters.st_spalnic !== null && property.st_spalnic !== filters.st_spalnic) return false;
-      if (filters.st_kopalnic !== null && property.st_kopalnic !== filters.st_kopalnic) return false;
-      if (filters.leto_izgradnje !== null && property.leto_izgradnje !== filters.leto_izgradnje) return false;
-      if (filters.st_nadstropij !== null && property.st_nadstropij !== filters.st_nadstropij) return false;
-      if (filters.velikost_zemljiscaMin !== null && velikostZemljisca < filters.velikost_zemljiscaMin) return false;
-      if (filters.velikost_zemljiscaMax !== null && velikostZemljisca > filters.velikost_zemljiscaMax) return false;
-      if (filters.velikost_skupajMin !== null && velikostSkupaj < filters.velikost_skupajMin) return false;
-      if (filters.velikost_skupajMax !== null && velikostSkupaj > filters.velikost_skupajMax) return false;
-      if (filters.agencija === "21Century" && property.agencija !== "21Century") return false;
-      if (filters.agencija === "Re-Max" && property.agencija !== "Re-Max") return false;
-      if (filters.agencija === "Drugo" && (property.agencija === "21Century" || property.agencija === "Re-Max")) return false;
+      if (filters.st_sob !== null && property.st_sob !== filters.st_sob)
+        return false;
+      if (
+        filters.st_spalnic !== null &&
+        property.st_spalnic !== filters.st_spalnic
+      )
+        return false;
+      if (
+        filters.st_kopalnic !== null &&
+        property.st_kopalnic !== filters.st_kopalnic
+      )
+        return false;
+      if (
+        filters.leto_izgradnje !== null &&
+        property.leto_izgradnje !== filters.leto_izgradnje
+      )
+        return false;
+      if (
+        filters.st_nadstropij !== null &&
+        property.st_nadstropij !== filters.st_nadstropij
+      )
+        return false;
+      if (
+        filters.velikost_zemljiscaMin !== null &&
+        velikostZemljisca < filters.velikost_zemljiscaMin
+      )
+        return false;
+      if (
+        filters.velikost_zemljiscaMax !== null &&
+        velikostZemljisca > filters.velikost_zemljiscaMax
+      )
+        return false;
+      if (
+        filters.velikost_skupajMin !== null &&
+        velikostSkupaj < filters.velikost_skupajMin
+      )
+        return false;
+      if (
+        filters.velikost_skupajMax !== null &&
+        velikostSkupaj > filters.velikost_skupajMax
+      )
+        return false;
+      if (filters.agencija === "21Century" && property.agencija !== "21Century")
+        return false;
+      if (filters.agencija === "Re-Max" && property.agencija !== "Re-Max")
+        return false;
+      if (
+        filters.agencija === "Drugo" &&
+        (property.agencija === "21Century" || property.agencija === "Re-Max")
+      )
+        return false;
       return true;
     });
   };
@@ -51,7 +101,10 @@ const FlatList = ({ filters = {} }) => {
   const filteredProperties = applyFilters(nepremicnine);
   const indexOfLastProperty = trenutnaStran * propertiesPerPage;
   const indexOfFirstProperty = indexOfLastProperty - propertiesPerPage;
-  const currentProperties = filteredProperties.slice(indexOfFirstProperty, indexOfLastProperty);
+  const currentProperties = filteredProperties.slice(
+    indexOfFirstProperty,
+    indexOfLastProperty
+  );
 
   const paginate = (pageNumber) => setTrenutnaStran(pageNumber);
 
@@ -60,7 +113,10 @@ const FlatList = ({ filters = {} }) => {
       <div className="container">
         <ul className="row">
           {currentProperties.map((nepremicnina) => (
-            <FlatItem key={nepremicnina.id_nepremicnine} nepremicnina={nepremicnina} />
+            <FlatItem
+              key={nepremicnina.id_nepremicnine}
+              nepremicnina={nepremicnina}
+            />
           ))}
         </ul>
 
@@ -70,7 +126,9 @@ const FlatList = ({ filters = {} }) => {
           }).map((_, index) => (
             <li
               key={index}
-              className={`page-item ${index + 1 === trenutnaStran ? "active" : ""}`}
+              className={`page-item ${
+                index + 1 === trenutnaStran ? "active" : ""
+              }`}
             >
               <button onClick={() => paginate(index + 1)} className="page-link">
                 {index + 1}
