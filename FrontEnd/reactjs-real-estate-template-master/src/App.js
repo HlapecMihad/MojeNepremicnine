a3ef4fc48ef4b516efc2c07c81092152df8a9cdc
carluka <carluka33@gmail.com>
2024-06-04 13:01:18 +0200

    popravki

--- FrontEnd/reactjs-real-estate-template-master/src/App.js
+++ FrontEnd/reactjs-real-estate-template-master/src/App.js
@@ -15,36 +15,33 @@ import SearchResult from "./components/SearchResult";
 import { NepremicnineProvider } from "./other/NepremicnineContext";
 import Registracija from "./components/Registracija";
 import Prijava from "./components/Prijava";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import FavoriteProperties from './components/FavoriteProperties';
-
-
-import { NepremicnineProvider } from "./other/NepremicnineContext";
+import "bootstrap/dist/css/bootstrap.min.css";
+import FavoriteProperties from "./components/FavoriteProperties";
 
 function App() {
   return (
-   <NepremicnineProvider>
-    <ComparisonProvider>
-      <Router>
-        <div className="App">
-          <Header />
-          <Switch>
-            <Route path="/" exact component={Home} />
-            <Route path="/primerjanje" component={ComparisonPage} />
-            <Route path="/about" component={About} />
-            <Route path="/blog" exact component={Blog} />
-            <Route path="/blog/:id" component={BlogDetail} />
-            <Route path="/nepremicnina/:id" component={FlatDetail} />
-            <Route path="/filtri" component={Filter} />
-            <Route path="/registracija" component={Registracija} />
-            <Route path="/prijava" component={Prijava} />
-            <Route path="/priljubljenje" component={FavoriteProperties} />
+    <NepremicnineProvider>
+      <ComparisonProvider>
+        <Router>
+          <div className="App">
+            <Header />
+            <Switch>
+              <Route path="/" exact component={Home} />
+              <Route path="/primerjanje" component={ComparisonPage} />
+              <Route path="/about" component={About} />
+              <Route path="/blog" exact component={Blog} />
+              <Route path="/blog/:id" component={BlogDetail} />
+              <Route path="/nepremicnina/:id" component={FlatDetail} />
+              <Route path="/filtri" component={Filter} />
+              <Route path="/registracija" component={Registracija} />
+              <Route path="/prijava" component={Prijava} />
+              <Route path="/priljubljenje" component={FavoriteProperties} />
               <Route path="/searchResult" component={SearchResult} />
-          </Switch>
-          <Footer />
-        </div>
-      </Router>
-    </ComparisonProvider>
+            </Switch>
+            <Footer />
+          </div>
+        </Router>
+      </ComparisonProvider>
     </NepremicnineProvider>
   );
 }
--- FrontEnd/reactjs-real-estate-template-master/src/components/Banner.js
+++ FrontEnd/reactjs-real-estate-template-master/src/components/Banner.js
@@ -5,80 +5,105 @@ import Filter from "./Filter";
 import FlatList from "./FlatList";
 
 const Banner = () => {
-    const [search, setSearch] = useState([]);
-    const [find, setFind] = useState([]);
-    const [word, setWord] = useState("");
-    const [showFilter, setShowFilter] = useState(false); // State for managing the Filter visibility
-    const [icon, setIcon] = useState("fa-filter"); // State for managing the icon
+  const [search, setSearch] = useState([]);
+  const [find, setFind] = useState([]);
+  const [word, setWord] = useState("");
+  const [showFilter, setShowFilter] = useState(false); // State for managing the Filter visibility
+  const [icon, setIcon] = useState("fa-filter"); // State for managing the icon
 
-    // Filter state
-    const [filters, setFilters] = useState({
-      posredovanje: "",
-      tip_nepremicnine: "",
-      lokacija: "",
-      cenaMin: null,
-      cenaMax: null,
-      st_sob: null,
-      st_spalnic: null,
-      st_kopalnic: null,
-      leto_izgradnje: null,
-      st_nadstropij: null,
-      velikost_zemljiscaMin: null,
-      velikost_zemljiscaMax: null,
-      velikost_skupajMin: null,
-      velikost_skupajMax: null,
-      agencija: ""
-    });
+  // Filter state
+  const [filters, setFilters] = useState({
+    posredovanje: "",
+    tip_nepremicnine: "",
+    lokacija: "",
+    cenaMin: null,
+    cenaMax: null,
+    st_sob: null,
+    st_spalnic: null,
+    st_kopalnic: null,
+    leto_izgradnje: null,
+    st_nadstropij: null,
+    velikost_zemljiscaMin: null,
+    velikost_zemljiscaMax: null,
+    velikost_skupajMin: null,
+    velikost_skupajMax: null,
+    agencija: "",
+  });
 
-    useEffect(() => {
-        setSearch(["a", "b", "test", "mb"]);
-    }, []);
-
+  useEffect(() => {
+    setSearch(["a", "b", "test", "mb"]);
+  }, []);
 
   const findSearch = (e) => {
     setWord(e.target.value);
   };
+  const handleFilterButtonClick = () => {
+    setShowFilter(!showFilter);
+    setIcon(showFilter ? "fa-filter" : "fa-times");
+  };
 
-    return (
-      <div>
-        <div className="banner d-flex align-items-center" style={{ backgroundImage: `url(${banner})` }}>
-            <div className="bg-custom">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-lg-6 mx-auto">
-                            <div className="banner-area text-center pt-4 pb-4">
-                                <p>Text</p>
-                                <h2 className="mt-2 mb-4 banner-title"><strong>NASLOV</strong></h2>
-                                <div className="search-area">
-                                    <button
-                                        className="btn-filter m-2"
-                                        title='Filtriraj'
-                                        onClick={handleFilterButtonClick}
-                                        style={{outline: 'none'}}
-                                    >
-                                        <i className={`fa ${icon}`} aria-hidden="true"></i>
-                                    </button>
-                                    <input
-                                        value={word}
-                                        onChange={(e) => findSearch(e)}
-                                        type="text"
-                                        className="inp-search"
-                                        placeholder="Išči..."
-                                    />
-                                    <Link to={`/searchResult?naziv=${word}`}>
-                                        <button className="btn-search m-2">Išči</button>
-                                    </Link>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+  const handleCloseFilter = () => {
+    setShowFilter(false);
+    setIcon("fa-filter");
+  };
+
+  const handleApplyFilters = (newFilters) => {
+    setFilters(newFilters);
+    setShowFilter(false);
+    setIcon("fa-filter");
+  };
+
+  return (
+    <div>
+      <div
+        className="banner d-flex align-items-center"
+        style={{ backgroundImage: `url(${banner})` }}
+      >
+        <div className="bg-custom">
+          <div className="container">
+            <div className="row">
+              <div className="col-lg-6 mx-auto">
+                <div className="banner-area text-center pt-4 pb-4">
+                  <p>Text</p>
+                  <h2 className="mt-2 mb-4 banner-title">
+                    <strong>NASLOV</strong>
+                  </h2>
+                  <div className="search-area">
+                    <button
+                      className="btn-filter m-2"
+                      title="Filtriraj"
+                      onClick={handleFilterButtonClick}
+                      style={{ outline: "none" }}
+                    >
+                      <i className={`fa ${icon}`} aria-hidden="true"></i>
+                    </button>
+                    <input
+                      value={word}
+                      onChange={(e) => findSearch(e)}
+                      type="text"
+                      className="inp-search"
+                      placeholder="Išči..."
+                    />
+                    <Link to={`/searchResult?naziv=${word}`}>
+                      <button className="btn-search m-2">Išči</button>
+                    </Link>
+                  </div>
                 </div>
+              </div>
             </div>
+          </div>
         </div>
-        {showFilter && <Filter filters={filters} onClose={handleCloseFilter} onApply={handleApplyFilters} />}
-        <FlatList filters={filters} />
       </div>
-    );
+      {showFilter && (
+        <Filter
+          filters={filters}
+          onClose={handleCloseFilter}
+          onApply={handleApplyFilters}
+        />
+      )}
+      <FlatList filters={filters} />
+    </div>
+  );
 };
 
 export default Banner;
